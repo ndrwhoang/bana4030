bc884457a583e8657947dc96163a3276e8e9f0c1
ndrwhoang <43328297+ndrwhoang@users.noreply.github.com>
2019-11-18 00:33:01 -0500

    ver1.0294.26b
    needs additional processing for keyword parameter

---
+++ mainscraping.py
@@ -0,0 +1,60 @@
+import pandas as pd
+from bs4 import BeautifulSoup
+import requests
+
+
+def indeed_job_scrape(keyword, search_location, no_page, job_type='None', exp_lvl='None'):
+    ### Data to scrape
+    # Job title
+    j_title = []
+    # Company name
+    company_name = []
+    # Salary
+    salary = []
+    # Location
+    location = []
+    # Rating
+    company_rating = []
+    # Posting link
+    hyperlink = []
+    # Posting description
+    j_desc = []
+    
+    ### Main scraping loop
+    for page_index in range(0, no_page*10, 10):
+        page = 'https://www.indeed.com/jobs?q=' + keyword + '&l=' + search_location + '&jt=' + job_type + '&explvl=' + exp_lvl + '&start=' + str(page_index)
+        page_response = requests.get(page, timeout=5)
+        main_soup = BeautifulSoup(page_response.text, 'html5lib')
+        for i in main_soup.find_all('div', {'class':'jobsearch-SerpJobCard'}):
+            # Position title
+            j_title.append(i.find('a', {'class':'jobtitle'})['title']) 
+            # Company name                       
+            company_name.append(i.find('span', {'class':'company'}).text)
+            # Salary (if information available, 'None' otherwise)                        
+            salary.append(i.find('span', {'class':'salaryText'}))  
+            # Job location                             
+            location.append(i.find(attrs={'class':'location'}).text)
+            # Comapny rating
+            company_rating.append(i.find('span', {'class':'ratingsContent'}).text if i.find('span', {'class':'ratingsContent'}) else 'None')
+            # Link to detailed job posting
+            hyperlink.append('https://www.indeed.com/' + str(i.find('a', {'class':'jobtitle'})['href']))
+            # Fulljob description
+            url = 'https://www.indeed.com/' + str(i.find('a', {'class':'jobtitle'})['href'])
+            url_response = requests.get(url, timeout=5)
+            soupy_soup = BeautifulSoup(url_response.text, 'html5lib')
+            j_desc.append(soupy_soup.find('div', {'id':'jobDescriptionText'}).text)
+    
+    ### Save to pandas dataframe 
+    df_local = pd.DataFrame({'job_title' : j_title,
+                       'company_name' : company_name,
+                       'salary' : salary,
+                       'job_location' : location,
+                       'direct_link' : hyperlink,
+                       'full_description' : j_desc})
+    return df_local
+
+# Scraping 2 pages of jobs information technology related in Ohio
+df = indeed_job_scrape('information+technology', 'Ohio', no_page=2)
+
+pd.options.display.max_columns = 50
+df.head()
